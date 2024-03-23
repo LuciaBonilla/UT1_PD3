d8a4b43893242d842f73551600382b686c6369d0
Lucía Bonilla <lucia.bonilla@correo.ucu.edu.uy>
2024-03-23 11:20:17 -0300

    Adición de prueba de clase 'Perro'

--- MainMaquinaB.java
+++ MainMaquinaB.java
@@ -1,8 +1,17 @@
 public final class MainMaquinaB {
+    /**
+     * Prueba básica de las clases {@link Perro} y {@link Gato}
+     * @param args
+     */
     public static void main(String[] args) {
         Gato gato = new Gato("Michi");
         System.out.println("El gato se llama Hercules? " + gato.respondeA("Hercules"));
         gato.maullar();
         System.out.println("El gato se llama " + gato.getNombre());
+
+        Perro perro = new Perro("Hercules");
+        System.out.println("El perro se llama Hercules? " + perro.respondeA("Hercules"));
+        perro.ladrar();
+        System.out.println("El perro se llama " + perro.getNombre());
     }
 }
