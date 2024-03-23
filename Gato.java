00771f46b91a28333fbcbde7609cde81b7b9831a
Lucía Bonilla <lucia.bonilla@correo.ucu.edu.uy>
2024-03-23 11:29:41 -0300

    Clase 'Gato' implementa interfaz 'Mamifero'

--- Gato.java
+++ Gato.java
@@ -1,4 +1,4 @@
-public class Gato {
+public class Gato implements Mamifero {
     private String nombre;
 
     public Gato(String nombre) {
@@ -12,5 +12,17 @@ public class Gato {
     }
     public boolean respondeA(String unNombre) {
         return false; //Nunca responde.
+    }
+
+    public void caminar() {
+        System.out.println("El gato camina...");
+    }
+
+    public void correr() {
+        System.out.println("El gato corre...");
+    }
+
+    public void saltar() {
+        System.out.println("El gato salta...");
     }
 }
